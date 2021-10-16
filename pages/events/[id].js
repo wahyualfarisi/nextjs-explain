03b6b21e1c4d7273d3928dd2dea923c6ac840669
wahyu alfarisi <wahyualfarisi@gmail.com>
2021-10-16 16:19:51 +0700

    add event detail

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,8 +1,35 @@
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+import { getEventById } from '../../dummy-data';
+
+//Component
+import EventSummary from './../../components/events/event-summary';
+import EventLogistics from './../../components/events/event-logistics';
+import EventContent from './../../components/events/event-content';
+
 function EventDetailPage(){
+    const router = useRouter();
+    const event  = getEventById(router.query.id);
+    
+
+    if(!event) {
+        return <p>Event Not Found !</p>
+    }
+
     return (
-        <div>
-            <h1>Event Detail Page</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics 
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+
+        </Fragment>
     )
 }
 
