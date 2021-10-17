1b834fe58b1f68c57f5979b454bab5d64a3458e7
wahyu alfarisi <wahyualfarisi@gmail.com>
2021-10-17 23:04:36 +0700

    combine with getServerSideProps

--- pages/user.js
+++ pages/user.js
@@ -1,33 +1,43 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
-function UserPage(){
-    const [isLoading, setIsLoading] = useState(false);
-    const [data, setData] = useState([]);
+function UserPage(props){
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [data, setData] = useState([]);
 
-    useEffect(() => {
-        setIsLoading(true);
-        fetch(`https://jsonplaceholder.typicode.com/users`,) // using SWR
-        .then(res => res.json())
-        .then(res => {
-            console.log(res);
-            setData(res)
-            setIsLoading(false);
-        })
-        .catch(err => {
-            console.log(err);
-            setIsLoading(false);
-        })
-    }, [])
+    // useEffect(() => {
+    //     setIsLoading(true);
+    //     fetch(`https://jsonplaceholder.typicode.com/users`,) // using SWR
+    //     .then(res => res.json())
+    //     .then(res => {
+    //         console.log(res);
+    //         setData(res)
+    //         setIsLoading(false);
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //         setIsLoading(false);
+    //     })
+    // }, [])
 
-    if(isLoading){
-        return <p>Loading...</p>
-    }
+    // if(isLoading){
+    //     return <p>Loading...</p>
+    // }
 
     return (
         <ul>
-            {data.map(item => <li key={item.id}>{item.name}</li>)}
+            {props.data.map(item => <li key={item.id}>{item.name}</li>)}
         </ul>
     )
+}
+
+export async function getServerSideProps(){
+    const getData = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const data = await getData.json();
+    return {
+        props: {
+            data: data ? data : []
+        }
+    }
 }
 
 export default UserPage;
