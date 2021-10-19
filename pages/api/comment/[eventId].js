5081e2a73d124cb9f2d7218532b11820f77549b0
wahyu alfarisi <wahyualfarisi@gmail.com>
2021-10-20 00:29:02 +0700

    create connection to mongodb

--- pages/api/comment/[eventId].js
+++ pages/api/comment/[eventId].js
@@ -1,24 +1,32 @@
-export default (req, res) => {
+import { MongoClient } from 'mongodb';
+
+export default async (req, res) => {
     const eventId = req.query.eventId;
+
+    const client = await MongoClient.connect(`mongodb+srv://nextjs:indonesiaku123@belajarmongodb.qxd4e.mongodb.net/events?retryWrites=true&w=majority`)
+    const db = client.db();
 
     switch(req.method){
 
         case 'POST':
             const { name, email, text } = req.body;
             const comment = {
-                id: new Date().toISOString(),
                 name,
                 email,
-                text
+                text,
+                eventId
             }
+            
+            const results = await db.collection('comments').insertOne(comment);
+            comment['id'] = results.insertedId;
+
             res.status(201).json({ message: 'Created Comment', data: comment })
         break;
 
         default:
-            const data = [
-                { id: 1, name: 'wahyu', email:'wahyualfarisi30@gmail.com', text: 'Great !'},
-                { id: 2, name: 'isyana', email:'isyana@gmail.com', text: 'Hallo !'},
-            ]
+            const data = await db.collection('comments').find().sort({ _id: 1 }).toArray();
             return res.status(200).json({ comments: data })
     }
+
+    client.close();
 }
--- pages/api/newslatter/index.js
+++ pages/api/newslatter/index.js
@@ -1,8 +1,16 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
 
     switch(req.method){
         case 'POST':
             const email = req.body.email;
+
+            const client = await MongoClient.connect(`mongodb+srv://nextjs:indonesiaku123@belajarmongodb.qxd4e.mongodb.net/events?retryWrites=true&w=majority`)
+            const db = client.db();
+            await db.collection('newslatter').insertOne({ email: email })
+            client.close();
+
             res.status(201).json({
                 email: email
             })
